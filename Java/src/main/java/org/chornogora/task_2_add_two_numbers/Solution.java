1c4051ff5bf24beb6d3736db6639a4d4a6c5375f
Chornogora <anthony.bulhakov@gmail.com>
2025-05-14 23:36:42 +0300

    [Task 2] Fixed task 2 (Java)

--- Java/src/main/java/org/chornogora/task_2_add_two_numbers/Solution.java
+++ Java/src/main/java/org/chornogora/task_2_add_two_numbers/Solution.java
@@ -3,8 +3,6 @@ package org.chornogora.task_2_add_two_numbers;
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode result = null;
-        l1 = reverseList(l1);
-        l2 = reverseList(l2);
         int reserved = 0;
         while (l1 != null || l2 != null) {
             if (l1 != null && l2 != null) {
