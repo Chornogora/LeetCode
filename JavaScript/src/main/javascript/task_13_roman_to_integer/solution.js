5f7de284a762387953404fca1f0f30a8ca12c78e
Chornogora <anthony.bulhakov@gmail.com>
2025-05-28 21:39:03 +0300

    [Task 13] Resolved Task 13 (JavaScript)

---
+++ JavaScript/src/main/javascript/task_13_roman_to_integer/solution.js
@@ -0,0 +1,28 @@
+var romanToInt = function (s) {
+    let dictionary = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+    for (let i = 0; i < s.length; ++i) {
+        if (i < s.length - 1 && dictionary[s[i + 1]] > dictionary[s[i]]) {
+            result += dictionary[s[i + 1]] - dictionary[s[i]];
+            ++i;
+        } else {
+            result += dictionary[s[i]]
+        }
+    }
+    return result;
+};
+
+console.log(romanToInt("III"));
+console.log(romanToInt("IV"));
+console.log(romanToInt("IX"));
+console.log(romanToInt("XIV"));
+console.log(romanToInt("XXI"));
